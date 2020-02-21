da8cf6d54eb7bdd524b891ef8ebdf1743c4ec58c
Hannah Zulueta <hannahjoy.zulueta@gmail.com>
2020-02-20 21:39:01 -0800

    Add menulinks

--- data/SiteConfig.js
+++ data/SiteConfig.js
@@ -13,16 +13,6 @@ const config = {
   userName: 'Hannah',
   userTwitter: 'homemadecoder', // Optionally renders "Follow Me" in the UserInfo segment.
   postsPerPage: 4, // Amount of posts displayed per listing page.
-  // siteRssTitle: 'Gatsby Advanced Starter RSS feed', // Title of the RSS feed
-  // siteFBAppID: '1825356251115265', // FB Application ID for using app insights
-  // googleAnalyticsID: 'UA-47311644-5', // GA tracking ID.
-  // disqusShortname: 'https-vagr9k-github-io-gatsby-advanced-starter', // Disqus shortname.
-  // userEmail: 'AdvancedUser@example.com', // Email used for RSS feed's author segment
-  // userLocation: 'North Pole, Earth', // User location to display in the author segment.
-  // userAvatar: 'https://api.adorable.io/avatars/150/test.png', // User avatar to display in the author segment.
-  // userDescription:
-  //   "Yeah, I like animals better than people sometimes... Especially dogs. Dogs are the best. Every time you come home, they act like they haven't seen you in a year. And the good thing about dogs... is they got different dogs for different people.", // User description to display in the author segment.
-  // Links to social profiles/projects you want to display in the author segment/navigation bar.
   userLinks: [
     {
       label: 'GitHub',
@@ -42,7 +32,31 @@ const config = {
   ],
   copyright: 'Copyright © 2020. Advanced User',
   themeColor: '#c62828', // Used for setting manifest and progress theme colors.
-  backgroundColor: '#e0e0e0' // Used for setting manifest background color.
+  backgroundColor: '#e0e0e0', // Used for setting manifest background color.
+  menuLinks: [
+    {
+      name: 'Me',
+      link: '/me/'
+    },
+    {
+      name: 'Blog',
+      link: '/blog/'
+    },
+    {
+      name: 'Contact',
+      link: '/contact/'
+    }
+  ]
+  // siteRssTitle: 'Gatsby Advanced Starter RSS feed', // Title of the RSS feed
+  // siteFBAppID: '1825356251115265', // FB Application ID for using app insights
+  // googleAnalyticsID: 'UA-47311644-5', // GA tracking ID.
+  // disqusShortname: 'https-vagr9k-github-io-gatsby-advanced-starter', // Disqus shortname.
+  // userEmail: 'AdvancedUser@example.com', // Email used for RSS feed's author segment
+  // userLocation: 'North Pole, Earth', // User location to display in the author segment.
+  // userAvatar: 'https://api.adorable.io/avatars/150/test.png', // User avatar to display in the author segment.
+  // userDescription:
+  //   "Yeah, I like animals better than people sometimes... Especially dogs. Dogs are the best. Every time you come home, they act like they haven't seen you in a year. And the good thing about dogs... is they got different dogs for different people.", // User description to display in the author segment.
+  // Links to social profiles/projects you want to display in the author segment/navigation bar.
 };
 
 // Validate
