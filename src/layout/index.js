b1c18b92a6d97d3d77b11ad6de42e0b92efdbb9f
Hannah Zulueta <hannahjoy.zulueta@gmail.com>
2020-02-22 19:17:50 -0800

    Install sass and add basic style structure

--- src/layout/index.js
+++ src/layout/index.js
@@ -2,6 +2,7 @@ import React from 'react';
 import Helmet from 'react-helmet';
 import config from '../../data/SiteConfig';
 import Navigation from '../components/Navigation';
+import '../styles/main.scss';
 
 export default class MainLayout extends React.Component {
   render() {
